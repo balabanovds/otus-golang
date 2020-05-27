38df706a6d7d49bc1977a92461691a5f52d92e31
Denis Balabanov <balabanov.ds@gmail.com>
2020-05-27 12:31:00 +0300

    HW1 done

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+type moment struct {
+	current time.Time
+	exact   time.Time
+}
+
+func newMoment() moment {
+	t, err := ntp.Time("ntp2.stratum2.ru")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return moment{
+		current: time.Now(),
+		exact:   t,
+	}
+}
+
+func (m moment) String() string {
+	return fmt.Sprintf("current time: %v\nexact time: %v",
+		m.current.Round(time.Second), m.exact.Round(time.Second))
+}
+
 func main() {
-	// Place your code here
+	m := newMoment()
+	fmt.Println(m)
 }
