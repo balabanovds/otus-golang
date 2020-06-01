978bdc91e545d6dac84827741bd22ab4f40abab0
Denis Balabanov <balabanov.ds@gmail.com>
2020-06-01 17:38:15 +0300

    add some tests

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -10,42 +10,58 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	keyA string = "aaa"
+	keyB string = "bbb"
+	keyC string = "ccc"
+	keyD string = "ddd"
+)
+
 func TestCache(t *testing.T) {
 	t.Run("empty cache", func(t *testing.T) {
 		c := NewCache(10)
 
-		_, ok := c.Get("aaa")
+		_, ok := c.Get(keyA)
 		require.False(t, ok)
 
-		_, ok = c.Get("bbb")
+		_, ok = c.Get(keyB)
 		require.False(t, ok)
+	})
+
+	t.Run("test clear cache", func(t *testing.T) {
+		c := initCache(t)
+
+		c.Clear()
+
+		_, ok := c.Get(keyA)
+		assert.False(t, ok)
 	})
 
 	t.Run("simple", func(t *testing.T) {
 		c := NewCache(5)
 
-		wasInCache := c.Set("aaa", 100)
+		wasInCache := c.Set(keyA, 100)
 		require.False(t, wasInCache)
 
-		wasInCache = c.Set("bbb", 200)
+		wasInCache = c.Set(keyB, 200)
 		require.False(t, wasInCache)
 
-		val, ok := c.Get("aaa")
+		val, ok := c.Get(keyA)
 		require.True(t, ok)
 		require.Equal(t, 100, val)
 
-		val, ok = c.Get("bbb")
+		val, ok = c.Get(keyB)
 		require.True(t, ok)
 		require.Equal(t, 200, val)
 
-		wasInCache = c.Set("aaa", 300)
+		wasInCache = c.Set(keyA, 300)
 		require.True(t, wasInCache)
 
-		val, ok = c.Get("aaa")
+		val, ok = c.Get(keyA)
 		require.True(t, ok)
 		require.Equal(t, 300, val)
 
-		val, ok = c.Get("ccc")
+		val, ok = c.Get(keyC)
 		require.False(t, ok)
 		require.Nil(t, val)
 	})
@@ -54,10 +70,10 @@ func TestCache(t *testing.T) {
 		t.Run("purge first element", func(t *testing.T) {
 			c := initCache(t)
 
-			was := c.Set("d", 4)
+			was := c.Set(keyD, 4)
 			assert.False(t, was)
 
-			a, ok := c.Get("a")
+			a, ok := c.Get(keyA)
 			assert.False(t, ok)
 			assert.Nil(t, a)
 		})
@@ -65,13 +81,13 @@ func TestCache(t *testing.T) {
 		t.Run("purge less used element", func(t *testing.T) {
 			c := initCache(t)
 
-			_ = c.Set("a", 4)
-			_, _ = c.Get("c")
+			_ = c.Set(keyA, 4)
+			_, _ = c.Get(keyC)
 
-			was := c.Set("d", 4)
+			was := c.Set(keyD, 4)
 			assert.False(t, was)
 
-			b, ok := c.Get("b")
+			b, ok := c.Get(keyB)
 			assert.False(t, ok)
 			assert.Nil(t, b)
 		})
@@ -107,14 +123,10 @@ func initCache(t *testing.T) Cache {
 
 	c := NewCache(3)
 
-	was := c.Set("a", 1)
-	assert.False(t, was)
-
-	was = c.Set("b", 2)
-	assert.False(t, was)
-
-	was = c.Set("c", 2)
-	assert.False(t, was)
+	for i, key := range [...]string{keyA, keyB, keyC} {
+		was := c.Set(key, i)
+		assert.False(t, was)
+	}
 
 	return c
 }
